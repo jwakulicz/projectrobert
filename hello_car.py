352a374cca6f6a820c39d44636e1deed85d1044b
Jen <jen.wakulicz@gmail.com>
2020-09-27 16:02:47 +1000

    added right turn

--- hello_car.py
+++ hello_car.py
@@ -25,8 +25,7 @@ p_right = GPIO.PWM(enB,1000)
 p_left.start(25)
 p_right.start(25)
 
-print("Default speed is low and forward.")
-print("To control the car, type go or stop.")
+print("To control the car, type forward, back, left, right, stop.")
 print("\n")
 
 while(1):
@@ -57,6 +56,15 @@ while(1):
         GPIO.output(in4,GPIO.LOW)
         p_left.ChangeDutyCycle(100)
         p_right.ChangeDutyCycle(25)
+        x = 'wait'
+    
+    elif x == 'right':
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in4,GPIO.LOW)
+        p_left.ChangeDutyCycle(25)
+        p_right.ChangeDutyCycle(100)
         x = 'wait'
 
     elif x == 'back':
