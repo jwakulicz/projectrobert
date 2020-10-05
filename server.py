67b16ef559d824caae42b0d497029b558df88210
timothywakulicz <tim.wakulicz@gmail.com>
2020-10-05 13:08:23 +1100

    added server.py

---
+++ server.py
@@ -0,0 +1,61 @@
+import RPi.GPIO as GPIO
+import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+host_name = '192.168.15.13'    # ip address of Pi
+host_port = 8000
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """ A special implementation of BaseHTTPRequestHander for reading data from
+        and control GPIO of a Raspberry Pi
+    """
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _redirect(self, path):
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', path)
+        self.end_headers()
+
+    def do_GET(self):
+        html = '''
+            <html>
+            <body style="width:960px; margin: 20px auto;">
+            <h1>Welcome to my Raspberry Pi</h1>
+            <p>Current GPU temperature is {}</p>
+            </body>
+            </html>
+        '''
+        temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
+        self.do_HEAD()
+        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])    # Get the size of data
+        post_data = self.rfile.read(content_length).decode("utf-8")   # Get the data
+        post_data = post_data.split("=")[1]    # Only keep the value
+        
+        # GPIO setup here
+
+        # Example of a POST request here
+        if post_data == 'Go':
+            #GPIO.output(18, GPIO.HIGH) make it go
+        else:
+            #GPIO.output(18, GPIO.LOW) stop
+
+        self._redirect('/')    # Redirect back to the root url
+
+if __name__ == '__main__':
+    http_server = HTTPServer((host_name, host_port), MyServer)
+    print("Server Starts - %s:%s" % (host_name, host_port))
+
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
